24b0b703698f27ac04022cf9776289cdeacaee7f
gavinwxy <xiaoyang.wong318@gmail.com>
2019-02-19 23:32:15 +0000

    func to solve transformation

---
+++ transform_solve.m
@@ -0,0 +1,32 @@
+function [t_mat, r_mat] = transform_solve(pc_a,pc_b)
+% This function is to calculate rotation and translation between
+% two set of n dimesion points. 
+%
+% args:
+%   pc_a: point set a with n points each of m dimensions
+%   pc_b: point set b with the same size as a
+% Return:
+%   t_mat: optimal translation matrix
+%   r_mat: optimal rotation matrix
+% In this case, pc_b = pc_a * r_mat + t_mat
+
+% Data centerlization
+dim_mean_a = mean(pc_a, 2);
+dim_mean_b = mean(pc_b, 2);
+ct_a = pc_a - dim_mean_a;
+ct_b = pc_b - dim_mean_b;
+
+% Calculate correlation matrix
+[dim_pc,num_pc] = size(pc_a);
+col_mat = zeros(dim_pc, num_pc);
+for i = 1:1:num_pc
+    tmp_mat = ct_a(:,i)*ct_b(:,i)';
+    col_mat = col_mat+tmp_mat;
+end
+
+% Singular value decomposition
+[U,~,V] = svd(col_mat);
+r_mat = V*U';
+t_mat = pc_b - pc_a*r_mat;
+
+return
