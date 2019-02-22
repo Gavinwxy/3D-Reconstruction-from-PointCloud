fedd9677742096c7e2aec0389501e7bd1edbdfc2
gavinwxy <xiaoyang.wong318@gmail.com>
2019-02-22 21:53:09 +0000

    Another version of SIFT pairs check

--- sift_match_transform/second_best_check.m
+++ sift_match_transform/second_best_check.m
@@ -1,30 +1,45 @@
 function sift_idx = second_best_check(sift_pairs, dist_collection, ratio_th)
-
+% Check the ratio between distances of best match and second best match
+% Args: 
+%   sift_pairs: Index of SIFT pairs in point cloud. [x1, y1; x2, y2]
+%   dist_collection: Corresponding distances
+%   ratio_th: Threshold ratio to judge if it is a valid match
+% Returns:
+%   sift_idx: Filtered SIFT pairs
+%
 idx_left = sift_pairs(:,1);
 idx_right = sift_pairs(:,2);
+
 idx_no_rep_left = unique(idx_left, 'stable');
-num_idx = length(idx_no_rep_left);
+num_idx_left = length(idx_no_rep_left);
+
 left_idx_temp = [];
 right_idx_temp = [];
+
 dist_temp = [];
 sift_idx = [];
 
-for i=1:1:num_idx
+for i=1:1:num_idx_left
     target_idx = idx_no_rep_left(i);
     target_loc = find(idx_left==target_idx);
     if length(target_loc)<2
+        left_idx_temp = [left_idx_temp; target_idx];
+        right_idx_temp = [right_idx_temp; idx_right(target_loc)];
+        dist_temp = [dist_temp; dist_collection(target_loc)];
         continue;
-    end
-    left = idx_left(target_loc);
-    right = idx_right(target_loc);
+    end    
+    left_extract = idx_left(target_loc);
+    right_extract = idx_right(target_loc);
     dist_target = dist_collection(target_loc);
+    
     [dist_sort, sort_idx] = sort(dist_target);
-    left = left(sort_idx);
-    right = right(sort_idx);
+    left_sort = left_extract(sort_idx);
+    right_sort = right_extract(sort_idx);
+    
     ratio = dist_sort(1)/dist_sort(2);
     if ratio < ratio_th
-        left_idx_temp = [left_idx_temp; left(1)];
-        right_idx_temp = [right_idx_temp; right(1)];
+        left_idx_temp = [left_idx_temp; left_sort(1)];
+        right_idx_temp = [right_idx_temp; right_sort(1)];
         dist_temp = [dist_temp; dist_sort(1)];
     else
         continue;
@@ -33,21 +48,23 @@ end
 
 idx_no_rep_right = unique(right_idx_temp, 'stable');
 num_idx_right = length(idx_no_rep_right);
+
 for i=1:1:num_idx_right
     target_idx = idx_no_rep_right(i);
     target_loc = find(right_idx_temp==target_idx);   
     if length(target_loc)<2
+        sift_idx = [sift_idx; left_idx_temp(target_loc) target_idx];
         continue;
     end
-    left = left_idx_temp(target_loc);
-    right = right_idx_temp(target_loc);
+    left_extract = left_idx_temp(target_loc);
+    right_extract = right_idx_temp(target_loc);
     dist_target = dist_temp(target_loc);
     [dist_sort, sort_idx] = sort(dist_target);
-    left = left(sort_idx);
-    right = right(sort_idx);
+    left_sort = left_extract(sort_idx);
+    right_sort = right_extract(sort_idx);
     ratio = dist_sort(1)/dist_sort(2);
     if ratio < ratio_th
-        sift_idx = [sift_idx; left(1) right(1)];
+        sift_idx = [sift_idx; left_sort(1) right_sort(1)];
     else
         continue;
     end
