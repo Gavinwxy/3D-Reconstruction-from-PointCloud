f2ba695258628b6692e2c26e30417ae9bd959fd5
Yanting Zeng <zengyt94@g.ucla.edu>
2019-02-23 21:54:33 +0000

    add comments to code

--- idx_convert_2d_to_1d.m
+++ idx_convert_2d_to_1d.m
@@ -1,4 +1,10 @@
 function idx_1d = idx_convert_2d_to_1d(idx_2d)
+% Convert 2D index to 1D index
+% Args: 
+%   idx_2d: a 2D index for a point in point cloud
+% Returns:
+%   idx_1d: a 1D index for the same point in point cloud
+
     X = 640;
     
     x = idx_2d(1);
--- mask_convert_1d_to_2d.m
+++ mask_convert_1d_to_2d.m
@@ -1,4 +1,10 @@
 function mask_2d = mask_convert_1d_to_2d(mask_1d)
+% Convert 1D list of discarded points' indexes to a 2D mask
+% Args:
+%   mask_1d: a 1D list of discarded points' indexes in a point cloud
+% Returns:
+%   mask_2d: 2D matrix indicating whether a pixel is valid or discarded
+
     X = 640;
     Y = 480;
     
